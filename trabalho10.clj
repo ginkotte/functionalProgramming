dfe0228a76a50b8e2750a2c42793d27fc3a5e5c0
Gustavo Inkotte <gus.inkotte@gmail.com>
2022-10-22 00:11:42 -0300

    Update trabalho10.clj

--- trabalho10.clj
+++ trabalho10.clj
@@ -4,7 +4,6 @@
 
 (println "Questão 1:")
 
-; 1.1 -> assoc - Tem relação com a função de map, retorna um map que ja tem o mapeamento de chave e valor.
 (println (format "Funcao assoc: Valor: %s; Resultado: %s"  "{:x 1 :y 2} :x 3" (str (assoc {:x 1 :y 2} :x 3))))
 (println (format "Funcao assoc: Valor: %s; Resultado: %s"  "{:x 2 :y 3} :z 1" (str (assoc {:x 2 :y 3} :z 1))))
 
@@ -39,9 +38,6 @@
 (println (format "Funcao sort: Valor: %s; Resultado: %s"  "[1 23 4345 5678 2]" (str (sort [1 23 4345 5678 2]))))
 (println (format "Funcao sort: Valor: %s; Resultado: %s"  " > [1 2 3]" (str (sort > [1 2 3]))))
 
-(println (format "Funcao partition-by: Valor: %s; Resultado: "  "odd? [1 1 2 2 2 3 3 3]") (partition-by odd? [1 1 2 2 3 3 3]))
-(println (format "Funcao partition-by: Valor: %s; Resultado: "  "even? [1 1 1 2 2 3]") (partition-by even? [1 1 1 2 2 3]))
-
 (println (format "Funcao empty: Valor: %s; Resultado: %s"  "empty? ()" (str (empty? ()))))
 (println (format "Funcao empty: Valor: %s; Resultado: %s"  "empty? (a)" (str (empty? '("a")))))
 
@@ -56,37 +52,27 @@
 (println "\nQuestão 2:")
 
 (defn ehPrimo [x]
-  (if (<= x 1)
-    false
-    (if (<= x 3)
-      true
-      (if (and (zero? (mod x 2)) (zero? (mod x 3)))
-        false
-        true))))
+  (loop [aux 1 arr []]
+    (if (= aux (+ x 1))
+      (if (= (count arr) 2) true false)
+      (recur (inc aux)
+             (if (zero? (mod x aux))
+               (conj arr aux) arr)))))
 
 (println (format "Funcao ehPrimo: Valor: %s; Resultado: %s"  "1" (str (ehPrimo 1))))
-(println (format "Funcao ehPrimo: Valor: %s; Resultado: %s"  "10" (str (ehPrimo 10))))
+(println (format "Funcao ehPrimo: Valor: %s; Resultado: %s"  "11" (str (ehPrimo 11))))
 
 ; 3) Utilizando  a  linguagem  Clojure,  crie  uma  função  chamada  fatoresPrimos  que  receba  um inteiro e devolva uma lista dos seus fatores primos. Decomposição em fatores primos é uma tarefa fundamental da aritmética.  
-
-(println "\nQuestão 3:")
-
-(defn ehPrimo [x]
-  (if (<= x 1)
-    false
-    (if (<= x 3)
-      true
-      (if (and ((mod x 2)) ((mod x 3)))
-        false
-        true))))
-
-(println (format "Função fatoresPrimos: Valor: %s; Resultado:"  "13") (fatoresPrimos 13))
-(println (format "Função fatoresPrimos: Valor: %s; Resultado:"  "12") (fatoresPrimos 12))
 
 ; 4) Utilizando  a  linguagem  Clojure,  crie  uma  função  chamada  todosPrimos  que  receba  dois valores inteiros e devolve todos os números primos que existam entre estes dois valores.  
 
 (println "\nQuestão 4:")
 
-(defn todosPrimos [x1 x2] (loop [l (range x1 (+ x2 1)) l2 []] (if (empty? (rest l)) l2 (recur (rest l) (if (ehPrimo (nth l 0)) (conj l2 (nth l 0)) l2)))))
+(defn todosPrimos [x y]
+  (loop [arr (range x (+ y 1)) arrAux []]
+    (if (empty? (rest arr)) arrAux
+        (recur (rest arr)
+               (if (ehPrimo (nth arr 0))
+                 (conj arrAux (nth arr 0)) arrAux)))))
 
 (println (format "Função todosPrimos: Valor: %s; Resultado:"  "2 13") (todosPrimos 2 13))
